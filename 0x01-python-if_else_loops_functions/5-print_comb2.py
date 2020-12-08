2f239f718bc486ea16e2909103dcac9653264082
nicolasportela <2103@holbertonschool.com>
2020-12-08 18:59:10 +0000

    task 5 fixed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for n in range(100):
-    if n < 10:
-        print("0{:d}, ".format(n), end="")
+    if n < 99:
+        print("{:02d}".format(n), end=", ")
     elif n == 99:
         print("{:d}".format(n))
-    else:
-        print("{:d}, ".format(n), end="")
