6ff0f77a8f471508b1a6a4d33032ff4ca094f5bd
nicolasportela <2103@holbertonschool.com>
2020-12-10 02:58:26 +0000

    task 15 fixed

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,6 +3,6 @@ def remove_char_at(str, n):
     for i in range(len(str)):
         if n > len(str) or n < 0:
             return str
-        elif 0 <= n <= len(str):
+        else:
             str = str[0:n] + str[n+1:]
             return str
