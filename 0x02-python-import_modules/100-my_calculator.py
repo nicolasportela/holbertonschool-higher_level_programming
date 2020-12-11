b025d1b4b3a13190d9247a678c037c095539dbc7
nicolasportela <2103@holbertonschool.com>
2020-12-11 05:08:28 +0000

    advanced task 6 fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,20 +5,21 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] is not "+" or not "-" or not "*" or not "/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
     else:
-        a = argv[1]
-        b = argv[3]
-        ai = int(a, 10)
-        bi = int(b, 10)
-        if argv[2] is "+":
-            res = add(ai, bi)
-        elif argv[2] is "-":
-            res = sub(ai, bi)
-        elif argv[2] is "*":
-            res = mul(ai, bi)
-        elif argv[2] is "/":
-            res = div(ai, bi)
-        print("{:d} {} {:d} = {:d}".format(ai, argv[2], bi, res))
+        if argv[2] is not "+" or not "-" or not "*" or not "/":
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        else:
+            a = argv[1]
+            b = argv[3]
+            ai = int(a, 10)
+            bi = int(b, 10)
+            if argv[2] is "+":
+                res = add(ai, bi)
+            elif argv[2] is "-":
+                res = sub(ai, bi)
+            elif argv[2] is "*":
+                res = mul(ai, bi)
+            elif argv[2] is "/":
+                res = div(ai, bi)
+            print("{:d} {} {:d} = {:d}".format(ai, argv[2], bi, res))
