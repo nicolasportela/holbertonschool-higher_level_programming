8f475fac076b39355149ed5d62c8233bded7b008
nicolasportela <2103@holbertonschool.com>
2020-12-16 04:04:25 +0000

    task 9 not finished and task 10

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,10 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    for i in range(len(my_list)):
+    for i in my_list:
         if i > 0:
-            if my_list[i] > my_list[i - 1]:
-                maxi = my_list[i]
+            if i > (i - 1):
+                maxi = i
+        else:
+            i += 1
     return maxi
