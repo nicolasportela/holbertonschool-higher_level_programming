fb0392b7a9fbe7f7ee3467832ae03a5746605f83
nicolasportela <2103@holbertonschool.com>
2020-12-18 02:48:46 +0000

    task 6 fixed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/paython3
 def print_sorted_dictionary(a_dictionary):
-    print(sorted(a_dictionary.keys())
+    for i in a_dictionary:
+        print("{:s}".format(sorted(a_dictionary.keys()))
