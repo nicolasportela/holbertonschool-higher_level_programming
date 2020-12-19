ca47b488a5f8f4eae9c40b95ff6a11404658a717
nicolasportela <2103@holbertonschool.com>
2020-12-19 23:32:48 +0000

    task 10 fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
 
-    for k, v in sorted(a_dictionary.items()):
-        maxi = k
-    return maxi
+    k_list = list(a_dictionary.keys())
+    v_list = list(a_dictionary.values())
+    maxv = v_list[0]
+    for v in v_list:
+        if v > maxv:
+            maxv = v
+    position = v_list.index(v)
+    maxk = k_list[position]
+    return maxk
