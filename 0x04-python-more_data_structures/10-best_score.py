2c8c3f1ef4f4b8f565db1fd45180b32975dde217
nicolasportela <2103@holbertonschool.com>
2020-12-19 12:19:10 +0000

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+
+    else:
+        for k, v in a_dictionary.items():
+            if v > (v - 1):
+                maxi = k
+                v = v + 1
+            else:
+                v = v + 1
+                continue
+        return maxi
