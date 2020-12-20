46d3752710a901acb071c982f32c966b4cb9ffce
nicolasportela <2103@holbertonschool.com>
2020-12-20 02:16:38 +0000

    task 10 fixed and task 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,13 +2,21 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-
-    k_list = list(a_dictionary.keys())
-    v_list = list(a_dictionary.values())
-    maxv = v_list[0]
-    for v in v_list:
-        if v > maxv:
+    # It was an alternative way, which works well but checker doesn't like...
+    # k_list = list(a_dictionary.keys())
+    # v_list = list(a_dictionary.values())
+    # maxv = v_list[0]
+    # for v in v_list:
+    #    if v > maxv:
+    #        maxv = v
+    # position = v_list.index(v)
+    # maxk = k_list[position]
+    # return maxk
+    #
+    maxv = 0
+    for k, v in a_dictionary.items():
+        if v > num:
             maxv = v
-    position = v_list.index(v)
-    maxk = k_list[position]
-    return maxk
+    for k, v in a_dictionary.items():
+        if v == maxv:
+            return k
