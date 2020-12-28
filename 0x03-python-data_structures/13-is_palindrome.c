e2c3ea68a49edaa39220ceeba11dddfac3f56cec
nicolasportela <2103@holbertonschool.com>
2020-12-28 04:59:14 +0000

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,3 +9,27 @@
 
 int is_palindrome(listint_t **head)
 {
+	listint_t *current = *head;
+	int array[2048];
+	int i = 0;
+	int j = 0;
+
+	if (head == NULL)
+		return (1);
+
+	while (current != NULL)
+	{
+		array[i] = current->n;
+		i++;
+		current = current->next;
+	}
+	i--;
+	while (j <= i)
+	{
+		if (array[j] != array[i])
+			return (0);
+		i--;
+		j++;
+	}
+	return (1);
+}
