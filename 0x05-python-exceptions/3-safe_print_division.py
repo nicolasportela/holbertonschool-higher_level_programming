380c8b9c2e7cc4266206420dfb8614ece667e6d6
nicolasportela <2103@holbertonschool.com>
2021-01-05 02:58:18 +0000

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+
+    except ArithmeticError:
+        result = None
+
+    finally:
+        print("Inside result:", end="")
+        print("{}".format(result))
+
+    return result
