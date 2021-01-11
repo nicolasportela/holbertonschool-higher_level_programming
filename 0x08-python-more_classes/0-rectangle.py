0c3ad1b0c379cd62a30fa5c2a8258939e26edb79
nicolasportela <2103@holbertonschool.com>
2021-01-11 21:21:26 +0000

    first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module
+write a class
+that defines a rectangle
+"""
+
+class Rectangle:
+    """it's an empty class"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module
+writes a class
+that defines a rectangle
+"""
+
+
+class Rectangle:
+    """based on 0-rectangle.py"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """setter"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """setter"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
