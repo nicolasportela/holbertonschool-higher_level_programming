49ff6dca995713d82a7daa8eb690f6320b8a3f29
nicolasportela <2103@holbertonschool.com>
2021-01-11 21:45:26 +0000

    fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,16 +10,8 @@ class Rectangle:
     """based on 0-rectangle.py"""
     def __init__(self, width=0, height=0):
         """init method"""
-        self.__width = width
-        self.__height = height
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
