e06ba46fdcd3eaa5767549b6c9793c09e24dd77c
nicolasportela <2103@holbertonschool.com>
2021-01-12 02:42:53 +0000

    task 8 fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,7 +75,7 @@ class Rectangle:
         Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
 
-    @biggest
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """compare area of two rectangles"""
         if not isinstance(rect_1, Rectangle):
