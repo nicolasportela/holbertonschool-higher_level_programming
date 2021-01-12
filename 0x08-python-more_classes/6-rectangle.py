95833494811cf4bfa3d33bc6a800cbf8532335a1
nicolasportela <2103@holbertonschool.com>
2021-01-12 01:08:49 +0000

    task 6 fix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_instances = number_of_instances + 1
+        Rectangle.number_of_instances = Rectangle.number_of_instances + 1
 
     @property
     def width(self):
@@ -71,5 +71,5 @@ class Rectangle:
 
     def __del__(self):
         """message when an instance of rectangle is deleted"""
-        Rectangle.number_of_instances = number_of_instances - 1
+        Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
