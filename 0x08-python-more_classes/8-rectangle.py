797ccd286f7ddd2b8c2d7e7ecc0bef4d601d4a0c
nicolasportela <2103@holbertonschool.com>
2021-01-12 02:34:40 +0000

    task 8 fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -11,22 +11,6 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
-    @biggest
-    def bigger_or_equal(rect_1, rect_2):
-        """compare area of two rectangles"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        area1 = rect_1.area()
-        area2 = rect_2.area()
-        if area1 == area2:
-            return rect_1
-        elif area1 > area2:
-            return rect_1
-        else:
-            return rect_2
-
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -90,3 +74,19 @@ class Rectangle:
         """message when an instance of rectangle is deleted"""
         Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
+
+        @biggest
+        def bigger_or_equal(rect_1, rect_2):
+            """compare area of two rectangles"""
+            if not isinstance(rect_1, Rectangle):
+                raise TypeError("rect_1 must be an instance of Rectangle")
+            if not isinstance(rect_2, Rectangle):
+                raise TypeError("rect_2 must be an instance of Rectangle")
+            area1 = rect_1.area()
+            area2 = rect_2.area()
+            if area1 == area2:
+                return rect_1
+            elif area1 > area2:
+                return rect_1
+            else:
+                return rect_2
