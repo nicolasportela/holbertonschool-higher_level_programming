b384a791b926e56feb5923705be5c58e3b199b5d
nicolasportela <2103@holbertonschool.com>
2021-01-18 02:42:02 +0000

    task 4 requirements fixed

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,14 +7,17 @@ with an int as size
 
 
 def print_square(size):
+    """
+    prints a square
+    """
     if type(size) is not int:
-        raise TypeError ("size must be an integer")
+        raise TypeError("size must be an integer")
     if type(size) is bool:
-        raise TypeError ("size must be an integer")
+        raise TypeError("size must be an integer")
     if type(size) is float and size < 0:
-        raise TypeError ("size must be an integer")
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError ("size must be >= 0")
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         for j in range(size):
