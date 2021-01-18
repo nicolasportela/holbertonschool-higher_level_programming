d0b7a39f09dbb72eda26cb9745e948a73cab8d78
nicolasportela <2103@holbertonschool.com>
2021-01-18 00:28:43 +0000

    task 0 fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,6 +7,7 @@ or raises TypeError if any of them are not int or float.
 
 
 def add_integer(a, b=98):
+    """adds 2 integers"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(a) is bool:
@@ -16,6 +17,14 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     if type(b) is bool:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
+
+    if type(a) is float('nan'):
+        a = 0
+    else:
+        a = int(a)
+    if type(b) is float('nan'):
+        b = 0
+    else:
+        b = int(b)
+    
     return a + b
