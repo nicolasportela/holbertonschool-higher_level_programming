e71684af6712c5906efe8fb3373309e33bec3960
nicolasportela <2103@holbertonschool.com>
2021-01-18 02:59:08 +0000

    task 3 fix

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -19,9 +19,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print()
-    if size == 0:
-        print()
+    if size >= 0:
+        for i in range(size):
+            print("#" * size)
