0a61cd2ce328466b12044ea480e62468f903a4b0
nicolasportela <2103@holbertonschool.com>
2021-01-20 20:52:22 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""script to add all arguments to a Python list
+and save them to a file"""
+
 
 import save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import json
 from sys import argv
 
+
+try:
+    json_list = load_from_json_file("add_item.json")
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, "add_item.json")
