870ab7757add9649610e88a2405aec03fe56f369
nicolasportela <2103@holbertonschool.com>
2021-01-20 19:53:59 +0000

    task 0 documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""this module contains a function to read a text file"""
 
 def read_file(filename=""):
+    """function to read a text file"""
 
     with open(filename, encoding="UTF8") as file:
         print(file.read())
