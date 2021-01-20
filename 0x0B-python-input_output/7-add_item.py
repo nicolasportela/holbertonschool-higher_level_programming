d41e196a958a70f57ea4e49c60112f7e6796b620
nicolasportela <2103@holbertonschool.com>
2021-01-20 20:58:12 +0000

    task 7 fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,6 @@ and save them to a file"""
 import save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import load_from_json_file = __import__('6-load_from_json_file').\
                              load_from_json_file
-import json
 from sys import argv
 
 
