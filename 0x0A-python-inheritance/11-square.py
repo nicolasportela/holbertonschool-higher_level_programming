1e12982bbad355fcf6884bfbc2f875cef44db935
nicolasportela <2103@holbertonschool.com>
2021-01-20 03:38:03 +0000

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module writes a derivated class"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """derivated from Rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
+        super().integer_validator("size", self.__size)
+
+    def area(self):
+        """calculates the area of the square"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """prints a string"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
