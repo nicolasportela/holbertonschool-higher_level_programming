e38b0ea64d81da39e90f244e7ea42beb86c724b6
nicolasportela <2103@holbertonschool.com>
2021-01-20 17:44:10 +0000

    tasks 0-6

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="UTF8") as file:
+        f = json.dumps(my_obj)
+        file.write(f)
