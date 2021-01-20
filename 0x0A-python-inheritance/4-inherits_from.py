2a3ef350e457dce8a1e1bafd9a030cea2096cafe
nicolasportela <2103@holbertonschool.com>
2021-01-20 02:03:30 +0000

    task 4 fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@
 def inherits_from(obj, a_class):
 
     """True if it is, False otherwise"""
-    return issubclass(obj, a_class) and type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
