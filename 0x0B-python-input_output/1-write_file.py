be6fba52d361343479d82464451f4a7b7fb6095a
nicolasportela <2103@holbertonschool.com>
2021-01-20 20:00:41 +0000

    task 1 documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""this module contains a function that writes a string to a text file"""
+
 
 def write_file(filename="", text=""):
+    """function to write to a text file"""
 
     with open(filename, "w", encoding="UTF8") as file:
         return file.write(text)
