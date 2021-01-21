fa10bee1da177590e0bc6f6d42e66e4e1f77f21a
nicolasportela <2103@holbertonschool.com>
2021-01-21 00:42:31 +0000

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this module writes a class named Student"""
+
+
+class Student:
+    """class creation"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+
+        self.first_name = first
+        self.last_name = last
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation"""
+
+        return self.__dict__
