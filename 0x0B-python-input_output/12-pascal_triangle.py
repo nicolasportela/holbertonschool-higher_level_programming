379f6da5b7a20e4d0483befbebd1b45737173952
nicolasportela <2103@holbertonschool.com>
2021-01-21 02:49:17 +0000

    task 12 not finished

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""this module contains a function that
+returns a list of lists of integers
+representing the Pascal triangle"""
+
+
+def pascal_triangle(n):
+    """function"""
+
+    for n in range(-100000, 1000000):
+        if n <= 0:
+            return lt = []
+        else:
+            
