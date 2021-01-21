96e093d819bf8293c753f02e6f1c13ab8c49d3d3
nicolasportela <2103@holbertonschool.com>
2021-01-21 00:49:35 +0000

    task 9 fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,11 +8,11 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """instantiation"""
 
-        self.first_name = first
-        self.last_name = last
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """retrieves a dictionary representation"""
 
-        return (self.__dict__)
+        return self.__dict__
