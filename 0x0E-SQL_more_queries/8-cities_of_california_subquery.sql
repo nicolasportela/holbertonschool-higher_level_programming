627ad04a354bb1791cb17f2c1b0b719e10d2e0e9
nicolasportela <2103@holbertonschool.com>
2021-03-02 21:51:42 +0000

    task 8 fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in the database
-SELECT id, name FROM hbtn_0d_usa.cities WHERE (name FROM hbtn_0d_usa.states WHERE name='California') ORDER BY cities.id ASC;
+SELECT id, name FROM hbtn_0d_usa.cities WHERE (SELECT name FROM hbtn_0d_usa.states WHERE name='California') ORDER BY cities.id ASC;
