03706718803a50887d04f244d8400047cf9ca5c9
nicolasportela <2103@holbertonschool.com>
2021-03-02 23:00:02 +0000

    task 10 edited

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
--- list all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all shows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
+ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
