4868fc9a6c624b4557a65b83b63edaca80196c4f
nicolasportela <2103@holbertonschool.com>
2021-03-22 15:30:07 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const arg = [];
 process.argv.forEach((val, index) => {
   arg[index] = `${val}`;
 });
-const number = Number(`${arg[2]}`)
+const number = Number(`${arg[2]}`);
 if (isNaN(number)) {
   console.log('Not a number');
 } else {
