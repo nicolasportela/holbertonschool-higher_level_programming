254c5890fb485838ed29e5874dcc8c91b503a511
nicolasportela <2103@holbertonschool.com>
2021-03-23 00:55:13 +0000

    task 11 fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const listarg = [];
-let i;
 process.argv.forEach((val, index) => {
   listarg[index] = `${val}`;
 });
 if (listarg.length <= 3) {
   console.log('0');
 } else {
-  listarg.sort(function(a, b){return a - b});
+  listarg.sort(function (a, b) { return a - b; });
   console.log(listarg[listarg.length - 2]);
 }
