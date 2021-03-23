5bea797af3e249d81b15d4d82e218b5641e46da0
nicolasportela <2103@holbertonschool.com>
2021-03-23 02:18:17 +0000

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n)) {
+    return (1);
+  }
+  if (n === 1) {
+    return (1);
+  }
+  return (n * factorial (n - 1));
+}
+const arg = [];
+process.argv.forEach((val, index) => {
+  arg[index] = `${val}`;
+});
+console.log(factorial(arg[2]));
