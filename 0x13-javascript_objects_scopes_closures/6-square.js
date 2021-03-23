98858aa7a7542bb7cc9d26e715b8f7560c4a0f8c
nicolasportela <2103@holbertonschool.com>
2021-03-23 21:28:34 +0000

    task 6 fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,9 @@ module.exports = class Square extends Square1 {
   charPrint (c) {
     let i;
     if (c === undefined) {
-      this.print();
+      for (i = 0; i < this.width; i++) {
+        console.log('X'.repeat(this.height));
+      } 
     } else {
       for (i = 0; i < this.width; i++) {
         console.log(c.repeat(this.height));
