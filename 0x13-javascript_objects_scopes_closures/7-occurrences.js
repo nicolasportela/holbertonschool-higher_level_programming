011abca1f8a3224e92b526119f0f99b970ef26f1
nicolasportela <2103@holbertonschool.com>
2021-03-24 21:09:32 +0000

    task 7 edited

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let cont = 0;
+  let count = 0;
   const newlist = list.filter(el => el === searchElement);
   for (let i = 0; i < newlist.length; i++) {
-    cont = cont + 1;
+    count = count + 1;
   }
   return (cont);
 };
