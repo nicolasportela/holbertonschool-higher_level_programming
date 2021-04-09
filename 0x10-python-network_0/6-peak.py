55e681a6175fc43c404ecb0f05e003c4f70e855c
nicolasportela <2103@holbertonschool.com>
2021-04-09 00:30:47 +0000

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,9 +3,23 @@
 
 
 def find_peak(list_of_integers):
-    if len(list_of_integers) < 3:
+    """function to return a peak number"""
+
+    n = len(list_of_integers)
+    if n == 0:
         return None
-    if len(list_of_integers) == 3:
-        return list_of_integers[2]
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
+
+    half_nodec = int(n / 2)
+    half_num = list_of_integers[half_nodec]
+
+    if half_num > list_of_integers[half_nodec - 1] and \
+       half_num > list_of_integers[half_nodec + 1]:
+        return half_num
+    elif half_num < list_of_integers[half_nodec - 1]:
+        return find_peak(list_of_integers[:half_nodec])
     else:
-        return max(list_of_integers[1:-1])
+        return find_peak(list_of_integers[half_nodec + 1:])
