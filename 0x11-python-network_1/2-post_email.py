bb16ca56296dde3f1721f14588a3b1296c2681a6
nicolasportela <2103@holbertonschool.com>
2021-04-09 20:52:59 +0000

    task 2 fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     import sys
 
     url = sys.argv[1]
-    values = {'email': sys.argv[2]}
+    values = {'email':sys.argv[2]}
 
     data = urllib.parse.urlencode(values)
     data = data.encode('utf8')
