d7742f8d4dc4a9cb9c65775c643a0bb08e500a61
nicolasportela <2103@holbertonschool.com>
2021-04-10 00:57:37 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(argv) > 1:
+        letter = argv[1]
+    else:
+        letter = ""
+    dic = {'q': letter}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=dic)
+    try:
+        result = r.json()
+        if 'id' in result and 'name' in result:
+            print("[{}] {}" .format(result['id'], result['name']))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
