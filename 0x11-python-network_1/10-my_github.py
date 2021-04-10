33f2a207b1f1d3c151aef03c434124b8b9fd7766
nicolasportela <2103@holbertonschool.com>
2021-04-10 01:35:21 +0000

    task 9 fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Python script that takes your GitHub credentials (username and password)
-and uses the GitHub API to display your id"""
+""" Python script that takes your GitHub credentials"""
 
 if __name__ == "__main__":
     import requests
