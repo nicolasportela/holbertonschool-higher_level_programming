c998fc5959c15eca91c12c68a0ec9894f77c65a9
nicolasportela <2103@holbertonschool.com>
2021-05-04 23:34:39 +0000

    task 6 fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,20 +2,20 @@
 const request = require('request');
 
 request.get(process.argv[2], function (err, response, body) {
-    if (err) {
-	console.log(err);
-    }
-    let completed = {};
-    let tasks = JSON.parse(body);
-    for (let i in tasks) {
-      let task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+  if (err) {
+    console.log(err);
+  }
+  const completed = {};
+  const tasks = JSON.parse(body);
+  for (const i in tasks) {
+    const task = tasks[i];
+    if (task.completed === true) {
+      if (completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      } else {
+        completed[task.userId]++;
       }
     }
-    console.log(completed);
+  }
+  console.log(completed);
 });
